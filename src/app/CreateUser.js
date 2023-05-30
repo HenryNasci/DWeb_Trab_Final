4233153c7dc2c6e7cea7dd3275136339d4ac96e7
Henrique Nascimento <aluno24173@ipt.pt>
2023-05-30 12:47:16 +0100

    createUser fetch

--- src/app/CreateUser.js
+++ src/app/CreateUser.js
@@ -1,34 +1,94 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './CreateUser.css';
-import logo from '../img/Logo_v2.png'
+import logo from '../img/Logo_v2.png';
 
-function CreateUser() {
-    return (
-        <div className="baseCreateUser">
-            <div className="CreateUser">
-                <img className='imgLogoCreateUser' alt='Imagem da Página CreateUser' src={logo} onClick={handleOnClickToMain}></img>
-                <div className='signIn'>
-                    <h1 style={{ color: 'white' }}>SIGN UP</h1>
-                    <form >
-                        <div className='divTxtCreateUser'>
-                            <b><p className='paragCreateUser'>Email:  </p></b>
-                            <b><p className='paragCreateUser'>Username:  </p></b>
-                            <b><p className='paragCreateUser'>Password:  </p></b>
-                            <b><p className='paragCreateUser'>Confirm Password:  </p></b>
-                        </div>
-                        <div className='divInputsCreateUser'>
-                            <input className='inputCreateUser' type='text' placeholder=''></input>
-                            <input className='inputCreateUser' type='text' placeholder=''></input>
-                            <input className='inputCreateUser' type='password' placeholder=''></input>
-                            <input className='inputCreateUser' type='password' placeholder=''></input>
-                        </div>
-                        <button className='buttonCreateUser' type='submit'>Sign Up</button>
-                        <a href='./Login' style={{color: 'white'}}>Already have an account?</a>
-                    </form>
+class CreateUser extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            email: '',
+            username: '',
+            password: '',
+            confirmPassword: ''
+        }
+    }
+
+    handleChangeEmail = (event) => {
+        this.setState({ email: event.target.value });
+    }
+
+    handleChangeUsername = (event) => {
+        this.setState({ username: event.target.value });
+    }
+
+    handleChangePasswd = (event) => {
+        this.setState({ password: event.target.value });
+    }
+
+    handleChangeConfPasswd = (event) => {
+        this.setState({ confirmPassword: event.target.value });
+    }
+
+    handleSubmit = (event) => {
+        event.preventDefault();
+        if (this.state.password === this.state.confirmPassword) {
+            fetch('https://localhost:7085/Clientes/Create', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({
+                    email: this.state.email,
+                    username: this.state.username,
+                    password: this.state.password
+                })
+            })
+                .then(response => response.json())
+                .then(data => {
+                    if (data.status === 200) {
+                        console.log('User created successfully!');
+                        window.location.href = './Login';
+                    } else {
+                        console.log('Error creating user!');
+                    }
+                })
+                .catch(error => {
+                    console.log(error);
+                });
+        } else {
+            console.log('Passwords do not match!');
+        }
+    }
+
+    render() {
+        return (
+            <div className="baseCreateUser">
+                <div className="CreateUser">
+                    <img className='imgLogoCreateUser' alt='Imagem da Página CreateUser' src={logo} onClick={handleOnClickToMain}></img>
+                    <div className='signIn'>
+                        <h1 style={{ color: 'white' }}>SIGN UP</h1>
+                        <form >
+                            <div className='divTxtCreateUser'>
+                                <b><p className='paragCreateUser'>Email:  </p></b>
+                                <b><p className='paragCreateUser'>Username:  </p></b>
+                                <b><p className='paragCreateUser'>Password:  </p></b>
+                                <b><p className='paragCreateUser'>Confirm Password:  </p></b>
+                            </div>
+                            <div className='divInputsCreateUser'>
+                                <input className='inputCreateUser' type='text' onChange={this.handleChangeEmail}></input>
+                                <input className='inputCreateUser' type='text' onChange={this.handleChangeUsername}></input>
+                                <input className='inputCreateUser' type='password' onChange={this.handleChangePasswd}></input>
+                                <input className='inputCreateUser' type='password' onChange={this.handleChangeConfPasswd}></input>
+                            </div>
+                            <button className='buttonCreateUser' type='submit' onClick={this.handleSubmit}>Sign Up</button>
+                            <a href='./Login' style={{color: 'white'}}>Already have an account?</a>
+                        </form>
+                    </div>
                 </div>
             </div>
-        </div>
-    )
+        )
+    }
 }
 
 function handleOnClickToMain() {
--- src/app/MainPage.js
+++ src/app/MainPage.js
@@ -1,11 +1,77 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './MainPage.css';
 import logo from '../img/Logo_v2.png';
 import name from '../img/nome2.png';
 import shopping_cart from '../img/shopping-cart.png';
 import Dropdown from 'react-bootstrap/Dropdown';
 
-function MainPage() {
+class MainPage extends Component {
+
+    
+    render() {
+        return (
+            <div className="baseMainPage">
+                <div className="headerMainPage">
+                    <div className="divLogoMainPage" onClick={handleOnClickToMain}>
+                        <img className='imgLogoMainPage' alt='Imagem da Página MainPage' src={logo} ></img>
+                        <img className='imgNameMainPage' alt='Imagem da Página MainPage' src={name} ></img>
+                    </div>
+                    <div className='divDropdownsMainPage'>
+                        <Dropdown className='dropdownMainPage'>
+                            <Dropdown.Toggle id="dropdown-button-dark-example1" variant="secondary">
+                                RIDER
+                            </Dropdown.Toggle>
+
+                            <Dropdown.Menu>
+                                <Dropdown.Item >...</Dropdown.Item>
+                                <Dropdown.Item >...</Dropdown.Item>
+                                <Dropdown.Item >...</Dropdown.Item>
+                            </Dropdown.Menu>
+                        </Dropdown>
+                        <Dropdown className='dropdownMainPage'>
+                            <Dropdown.Toggle id="dropdown-button-dark-example1" variant="secondary">
+                                HORSE
+                            </Dropdown.Toggle>
+
+                            <Dropdown.Menu>
+                                <Dropdown.Item >...</Dropdown.Item>
+                                <Dropdown.Item >...</Dropdown.Item>
+                                <Dropdown.Item >...</Dropdown.Item>
+                            </Dropdown.Menu>
+                        </Dropdown> 
+                        <Dropdown className='dropdownMainPage'>
+                            <Dropdown.Toggle id="dropdown-button-dark-example1" variant="secondary">
+                                STABLE
+                            </Dropdown.Toggle>
+
+                            <Dropdown.Menu>
+                                <Dropdown.Item >...</Dropdown.Item>
+                                <Dropdown.Item >...</Dropdown.Item>
+                                <Dropdown.Item >...</Dropdown.Item>
+                            </Dropdown.Menu>
+                        </Dropdown> 
+                        <Dropdown className='dropdownMainPage'>
+                            <Dropdown.Toggle id="dropdown-button-dark-example1" variant="secondary">
+                                BRANDS
+                            </Dropdown.Toggle>
+
+                            <Dropdown.Menu>
+                                <Dropdown.Item >...</Dropdown.Item>
+                                <Dropdown.Item >...</Dropdown.Item>
+                                <Dropdown.Item >...</Dropdown.Item>
+                            </Dropdown.Menu>
+                        </Dropdown>
+                    </div>
+                    <div className='btnLog'>
+                        <img className='imgSoppingCart' alt='Imagem do carrinho de compras' src={shopping_cart} ></img>
+                        <button className="buttonSignMainPage" onClick={handleOnClickToSign}>Sign up</button>
+                        <button className="buttonLogMainPage" onClick={handleOnClickToLog}>Login</button>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
 
     function handleOnClickToLog() {
         window.location.href = './Login';
@@ -18,70 +84,6 @@ function MainPage() {
     function handleOnClickToMain() {
         window.location.href = './';
     }
-    
-    return (
-        <div className="baseMainPage">
-            <div className="headerMainPage">
-                <div className="divLogoMainPage" onClick={handleOnClickToMain}>
-                    <img className='imgLogoMainPage' alt='Imagem da Página MainPage' src={logo} ></img>
-                    <img className='imgNameMainPage' alt='Imagem da Página MainPage' src={name} ></img>
-                </div>
-                <div className='divDropdownsMainPage'>
-                    <Dropdown className='dropdownMainPage'>
-                        <Dropdown.Toggle id="dropdown-button-dark-example1" variant="secondary">
-                            RIDER
-                        </Dropdown.Toggle>
-
-                        <Dropdown.Menu>
-                            <Dropdown.Item >...</Dropdown.Item>
-                            <Dropdown.Item >...</Dropdown.Item>
-                            <Dropdown.Item >...</Dropdown.Item>
-                        </Dropdown.Menu>
-                    </Dropdown>
-                    <Dropdown className='dropdownMainPage'>
-                        <Dropdown.Toggle id="dropdown-button-dark-example1" variant="secondary">
-                            HORSE
-                        </Dropdown.Toggle>
-
-                        <Dropdown.Menu>
-                            <Dropdown.Item >...</Dropdown.Item>
-                            <Dropdown.Item >...</Dropdown.Item>
-                            <Dropdown.Item >...</Dropdown.Item>
-                        </Dropdown.Menu>
-                    </Dropdown> 
-                    <Dropdown className='dropdownMainPage'>
-                        <Dropdown.Toggle id="dropdown-button-dark-example1" variant="secondary">
-                            STABLE
-                        </Dropdown.Toggle>
-
-                        <Dropdown.Menu>
-                            <Dropdown.Item >...</Dropdown.Item>
-                            <Dropdown.Item >...</Dropdown.Item>
-                            <Dropdown.Item >...</Dropdown.Item>
-                        </Dropdown.Menu>
-                    </Dropdown> 
-                    <Dropdown className='dropdownMainPage'>
-                        <Dropdown.Toggle id="dropdown-button-dark-example1" variant="secondary">
-                            BRANDS
-                        </Dropdown.Toggle>
-
-                        <Dropdown.Menu>
-                            <Dropdown.Item >...</Dropdown.Item>
-                            <Dropdown.Item >...</Dropdown.Item>
-                            <Dropdown.Item >...</Dropdown.Item>
-                        </Dropdown.Menu>
-                    </Dropdown>
-                </div>
-                <div className='btnLog'>
-                    <img className='imgSoppingCart' alt='Imagem do carrinho de compras' src={shopping_cart} ></img>
-                    <button className="buttonSignMainPage" onClick={handleOnClickToSign}>Sign up</button>
-                    <button className="buttonLogMainPage" onClick={handleOnClickToLog}>Login</button>
-                </div>
-            </div>
-        </div>
-    );
-}
-
 
 export default MainPage;
 
