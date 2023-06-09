5b9c8c119c95074df0bb43097cab64b2ad04e068
AndreValaa <andremvala@gmail.com>
2023-06-10 00:00:24 +0100

    dropdown categories

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -6,85 +6,87 @@ import Dropdown from 'react-bootstrap/Dropdown';
 
 function Header() {
 
-    const handleOnClickToLog = () => {
-        window.location.href = './Login';
-    };
+  const handleOnClickToLog = () => {
+    window.location.href = './Login';
+  };
 
-    const handleOnClickToSign = () => {
-        window.location.href = './CreateUser';
-    };
+  const handleOnClickToSign = () => {
+    window.location.href = './CreateUser';
+  };
 
-    const handleOnClickToMain = () => {
-        window.location.href = './';
-    };
+  const handleOnClickToMain = () => {
+    window.location.href = './';
+  };
 
-    return(
-      <div className="headerMainPage">
-        <div className="divLogoMainPage" onClick={handleOnClickToMain}>
-          <img className="imgLogoMainPage" alt="Imagem da Página MainPage" src={logo} />
-          <img className="imgNameMainPage" alt="Imagem da Página MainPage" src={name} />
-        </div>
-        <div className="divDropdownsMainPage">
-          <Dropdown className="dropdownMainPage">
-            <Dropdown.Toggle id="dropdown-button-dark-example1" variant="secondary">
-              RIDER
-            </Dropdown.Toggle>
-
-            <Dropdown.Menu>
-              <Dropdown.Item>...</Dropdown.Item>
-              <Dropdown.Item>...</Dropdown.Item>
-              <Dropdown.Item>...</Dropdown.Item>
-            </Dropdown.Menu>
-          </Dropdown>
-          <Dropdown className="dropdownMainPage">
-            <Dropdown.Toggle id="dropdown-button-dark-example1" variant="secondary">
-              HORSE
-            </Dropdown.Toggle>
-
-            <Dropdown.Menu>
-              <Dropdown.Item>Saddle</Dropdown.Item>
-              <Dropdown.Item>Saddle Pads</Dropdown.Item>
-              <Dropdown.Item>Tendon Boots</Dropdown.Item>
-              <Dropdown.Item>Rugs</Dropdown.Item>
-              <Dropdown.Item>Bridles</Dropdown.Item>
-            </Dropdown.Menu>
-          </Dropdown>
-          <Dropdown className="dropdownMainPage">
-            <Dropdown.Toggle id="dropdown-button-dark-example1" variant="secondary">
-              STABLE
-            </Dropdown.Toggle>
-
-            <Dropdown.Menu>
-              <Dropdown.Item>...</Dropdown.Item>
-              <Dropdown.Item>...</Dropdown.Item>
-              <Dropdown.Item>...</Dropdown.Item>
-            </Dropdown.Menu>
-          </Dropdown>
-          <Dropdown className="dropdownMainPage">
-            <Dropdown.Toggle id="dropdown-button-dark-example1" variant="secondary">
-              BRANDS
-            </Dropdown.Toggle>
-
-            <Dropdown.Menu>
-              <Dropdown.Item>Horze</Dropdown.Item>
-              <Dropdown.Item>Jumpin</Dropdown.Item>
-              <Dropdown.Item>Kingsland</Dropdown.Item>
-              <Dropdown.Item>Harcour</Dropdown.Item>
-              <Dropdown.Item>Eskadron</Dropdown.Item>
-            </Dropdown.Menu>
-          </Dropdown>
-        </div>
-        <div className="btnLog">
-          <img className="imgSoppingCart" alt="Imagem do carrinho de compras" src={shopping_cart} />
-          <button className="buttonSignMainPage" onClick={handleOnClickToSign}>
-            Sign up
-          </button>
-          <button className="buttonLogMainPage" onClick={handleOnClickToLog}>
-            Login
-          </button>
-        </div>
+  return (
+    <div className="headerMainPage">
+      <div className="divLogoMainPage" onClick={handleOnClickToMain}>
+        <img className="imgLogoMainPage" alt="Imagem da Página MainPage" src={logo} />
+        <img className="imgNameMainPage" alt="Imagem da Página MainPage" src={name} />
       </div>
-    )
+      <div className="divDropdownsMainPage">
+        <Dropdown className="dropdownMainPage">
+          <Dropdown.Toggle id="dropdown-button-dark-example1" variant="secondary">
+            RIDER
+          </Dropdown.Toggle>
+
+          <Dropdown.Menu>
+            <Dropdown.Item>Boots</Dropdown.Item>
+            <Dropdown.Item>Helmets</Dropdown.Item>
+            <Dropdown.Item>Breeches</Dropdown.Item>
+            <Dropdown.Item>Belts</Dropdown.Item>
+            <Dropdown.Item>Jackets</Dropdown.Item>
+          </Dropdown.Menu>
+        </Dropdown>
+        <Dropdown className="dropdownMainPage">
+          <Dropdown.Toggle id="dropdown-button-dark-example1" variant="secondary">
+            HORSE
+          </Dropdown.Toggle>
+
+          <Dropdown.Menu>
+            <Dropdown.Item>Saddle</Dropdown.Item>
+            <Dropdown.Item>Saddle Pads</Dropdown.Item>
+            <Dropdown.Item>Tendon Boots</Dropdown.Item>
+            <Dropdown.Item>Rugs</Dropdown.Item>
+            <Dropdown.Item>Bridles</Dropdown.Item>
+          </Dropdown.Menu>
+        </Dropdown>
+        <Dropdown className="dropdownMainPage">
+          <Dropdown.Toggle id="dropdown-button-dark-example1" variant="secondary">
+            STABLE
+          </Dropdown.Toggle>
+
+          <Dropdown.Menu>
+            <Dropdown.Item>Forks</Dropdown.Item>
+            <Dropdown.Item>Shovels</Dropdown.Item>
+            <Dropdown.Item>Hay nets</Dropdown.Item>
+          </Dropdown.Menu>
+        </Dropdown>
+        <Dropdown className="dropdownMainPage">
+          <Dropdown.Toggle id="dropdown-button-dark-example1" variant="secondary">
+            BRANDS
+          </Dropdown.Toggle>
+
+          <Dropdown.Menu>
+            <Dropdown.Item>Horze</Dropdown.Item>
+            <Dropdown.Item>Jumpin</Dropdown.Item>
+            <Dropdown.Item>Kingsland</Dropdown.Item>
+            <Dropdown.Item>Harcour</Dropdown.Item>
+            <Dropdown.Item>Eskadron</Dropdown.Item>
+          </Dropdown.Menu>
+        </Dropdown>
+      </div>
+      <div className="btnLog">
+        <img className="imgSoppingCart" alt="Imagem do carrinho de compras" src={shopping_cart} />
+        <button className="buttonSignMainPage" onClick={handleOnClickToSign}>
+          Sign up
+        </button>
+        <button className="buttonLogMainPage" onClick={handleOnClickToLog}>
+          Login
+        </button>
+      </div>
+    </div>
+  )
 }
 
 export default Header
